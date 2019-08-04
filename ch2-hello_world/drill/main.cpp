357a0e5f5b9a1219852c30779edb3b0e7b34578d
Franz Pucher <franz.pucher@gmail.com>
2019-08-04 21:22:27 +0200

    adds drill

---
+++ ch2-hello_world/drill/main.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//#include "../../std_lib_facilities.h"
+#include "std_lib_facilities.h"
+//#include "std_facilities.h"
+
+int main() // C++ programs start by executing the function main
+{
+    cout << "Hello, World!\n"; // output “Hello, World!”
+    //keep_window_open(); // wait for a character to be entered
+    return 0;
+}

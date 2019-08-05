fb5cc6918d09def06a0f60798e0f9bec9c85eafc
Franz Pucher <franz.pucher@gmail.com>
2019-08-05 21:59:37 +0200

    adds solution to first exercise

---
+++ ch2-hello_world/exercises/exercise-01/main.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main() // C++ programs start by executing the function main
+{
+    cout << "Hello, programming!\n"; // output “Hello, World!”
+    cout << "Here we go!\n"; // output “Hello, World!”
+    keep_window_open(); // wait for a character to be entered
+    return 0;
+}

098502daa2fe9dd2d888f4a89894b12bc07c74ba
Franz Pucher <franz.pucher@gmail.com>
2019-08-05 21:59:19 +0200

    updates drill

--- ch2-hello_world/drill/main.cpp
+++ ch2-hello_world/drill/main.cpp
@@ -6,6 +6,6 @@
 int main() // C++ programs start by executing the function main
 {
     cout << "Hello, World!\n"; // output “Hello, World!”
-    //keep_window_open(); // wait for a character to be entered
+    keep_window_open(); // wait for a character to be entered
     return 0;
 }

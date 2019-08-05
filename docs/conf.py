cea9e9aa47faed9cef38627121670a8afc78e546
Franz Pucher <franz.pucher@gmail.com>
2019-08-06 00:04:59 +0200

    removes recommonmark

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx_rtd_theme',
-    'recommonmark',
+    #'recommonmark',
     'm2r']
 
 # Add any paths that contain templates here, relative to this directory.

fb4ea5eed6594f9714d23727e57824dabe242a3e
Franz Pucher <franz.pucher@gmail.com>
2019-08-08 22:42:22 +0200

    adds exercise 04 solution

---
+++ ch2-hello_world/exercises/exercise04/README.md
@@ -0,0 +1,11 @@
+### Exercise 04
+
+Find a good cookbook. Read the instructions for baking blueberry muffins (if you are in a country where “blueberry muffins” is a strange, exotic dish, use a more familiar dish instead). 
+Please note that with a bit of help and instruction, most of the people in the world can bake delicious blueberry muffins. It is not considered advanced or difficult fine cooking. 
+However, for the author, few exercises in this book are as difficult as this one. It is amazing what you can do with a bit of practice.
+
+Rewrite those instructions so that each individual action is in its own numbered paragraph. Be careful to list all ingredients and all kitchen utensils used at each step. 
+Be careful about crucial details, such as the desired oven temperature, preheating the oven, the preparation of the muffin pan, the way to time the cooking, and the need to protect your hands when removing the muffins from the oven.
+Consider those instructions from the point of view of a cooking novice (if you are not one, get help from a friend who does not know how to cook). Fill in the steps that the book’s author (almost certainly an experienced cook) left out for being obvious.
+Build a glossary of terms used. (What’s a muffin pan? What does preheating do? What do you mean by “oven”?)
+Now bake some muffins and enjoy your results.
---
+++ ch2-hello_world/exercises/exercise04/main.cpp
@@ -0,0 +1,75 @@
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+    cout << "Instructions for approximately 12 classical blueberry muffins that are easy to take with you:\n";
+    cout << "1. Definitions:\n";
+    cout << "2. Define bowl.\n";
+    cout << "3. Define oven and pre heating it until a temperature is reached.\n";
+    cout << "4. Define mixer and its usage (fast, slow mixing).\n";
+    cout << "4. Define other kitchen utensils and how to use them (bowl, muffin pan, mixer, ...).\n";
+    cout << "6. Define ingredients.\n";
+    cout << "5. Define what a muffin pan is an how to grease it.\n";
+    cout << "7. Define how to open packaging (flour, baking soda pack, sugar, ...).\n";
+    cout << "8. Define how to take berries out of the packaging.\n";
+    cout << "9. Define how to peel a lemon.\n";
+    cout << "10. Define how to wash berries.\n";
+    cout << "11. Define how to put berries to the dough.\n";
+    cout << "12. Define how to smash an egg open into a bowl.\n";
+    cout << "13. Ingredients.\n";
+    cout << "14. 200 g blueberries (fresh, out of the glas or the freezer).\n";
+    cout << "15. 2 El flour.\n";
+    cout << "16. 250 g flour (Type 405).\n";
+    cout << "17. 2 TL backing soda.\n";
+    cout << "18. 1/2 TL Natron g blueberries.\n";
+    cout << "19. 1 TL lemon peel from one untreated lemon.\n";
+    cout << "20. 1 egg.\n";
+    cout << "21. 120 g brown or white sugar.\n";
+    cout << "22. 1 pack vanilla sugar.\n";
+    cout << "23. 80 ml neutral oil or 125 g soft butter.\n";
+    cout << "24. 200 g butter milk.\n";
+    cout << "25. For the muffin pan.\n";
+    cout << "26. Fat or 12 paper cups.\n";
+    cout << "27. Preparation time: 15 min.\n";
+    cout << "28. Baking time: 20-25 min.\n";
+    cout << "29. Per muffing approx.: 170 kcal, 4 g EW/5 g, F/29 g KH .\n";
+    cout << "30. Preparation time: 15 min.\n";
+    cout << "30. Actions:\n";
+    cout << "32. 1:\n";
+    cout << "33. Pre heat the oven to 180 degrees celsius.\n";
+    cout << "34. Prepare the muffin pan using a small amount of butter to grease the moulds.\n";
+    cout << "35. 2:\n";
+    cout << "36. Wash fresh blueberries and choose the best ones.\n";
+    cout << "37. Make sure the blueberries are dried well by putting them in a kitchen roll.\n";
+    cout << "38. If you choose to use berries from the freezer, take them out but don't let them thaw.\n";
+    cout << "39. Sprinkle flour through a sieve on the blueberries.\n";
+    cout << "40. 3.\n";
+    cout << "41. Put 250 g flour (Type 405) in a bowl.\n";
+    cout << "42. Put backing soda, natron and lemon peel inside the bowl.\n";
+    cout << "43. Mix the content in the bowl.\n";
+    cout << "44. 4.\n";
+    cout << "45. Take another bowl.\n";
+    cout << "46. Smash one egg and put it inside the new bowl.\n";
+    cout << "47. Whisk the egg inside the bowl.\n";
+    cout << "48. Put 120 g white or brown sugar inside the new bowl to the egg.\n";
+    cout << "49. Put one pack of vanilla sugar inside the new bowl.\n";
+    cout << "50. Add 80 ml neutral oil or 125 g soft butter into the new bowl.\n";
+    cout << "51. Add 200 g butter milk into the new bowl.\n";
+    cout << "52. Stir the content inside the new bowl well.\n";
+    cout << "53. 5.\n";
+    cout << "54. Mix the flour mix in the egg mix and stir shortly.\n";
+    cout << "55. Stir until the dry ingredients are wet.\n";
+    cout << "56. Carefully fold the blueberries into the mix.\n";
+    cout << "57. 6.\n";
+    cout << "58. Fill the dough into the moulds of the muffin pan.\n";
+    cout << "59. Put the muffin pan inside the ofen (middle, circulating air at 160 degree celsius).\n";
+    cout << "60. Bake for 20 to 25 min by setting a timer.\n";
+    cout << "61. After the timer fires, take the muffins out of the oven onto the kitchen line.\n";
+    cout << "62. Let the muffins rest inside muffin pan for 5 min.\n";
+    cout << "63. After the 5 min timer fires, take the muffins out of the muffin pan.\n";
+    cout << "64. Serve while the muffins are warm.\n";
+    cout << "65. Enjoy the fresh blueberry muffins.\n";
+    keep_window_open(); // wait for a character to be entered
+    return 0;
+}

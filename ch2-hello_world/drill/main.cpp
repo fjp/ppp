b380feccfc704550a1e8b9ac046c2c51c78db5ff
F Pucher <franz.pucher@gmail.com>
2019-08-18 22:02:44 +0200

    Update main.cpp

--- ch2-hello_world/drill/main.cpp
+++ ch2-hello_world/drill/main.cpp
@@ -1,7 +1,4 @@
-//#include <iostream>
-//#include "../../std_lib_facilities.h"
 #include "std_lib_facilities.h"
-//#include "std_facilities.h"
 
 int main() // C++ programs start by executing the function main
 {

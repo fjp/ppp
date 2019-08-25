b3d7bbce736a84ff6ac7698165ce9fbd663f475f
Franz Pucher <franz.pucher@gmail.com>
2019-08-25 16:18:14 +0200

    adds solution to first exercise
    solution for miles to kilometers exercise

--- ch3-objects_types_and_values/exercises/exercise01/main.cpp
+++ ch3-objects_types_and_values/exercises/exercise01/main.cpp
@@ -3,5 +3,23 @@
 
 int main()
 {
+    cout << "Enter a number of miles (followed by 'Enter'):\n";
+    double miles;
+    cin >> miles;
+    const double kilometersToMile = 1.609;
+    if (1.0 == miles)
+    {
+        cout << miles << " mile is equal to " << miles * kilometersToMile << " kilometers.\n";
+    }
+    else {
+        double kilometers = miles * kilometersToMile;
+        if (1.0 == kilometers) {
+            cout << miles << " miles are equal to " << kilometers << " kilometer.\n";
+        }
+        else {
+            cout << miles << " miles are equal to " << kilometers << " kilometers.\n";
+        }
+    }
+
     return 0;
 }

2b49918be57afd991e8f4fbf44d01a4015a7b224
Franz Pucher <franz.pucher@gmail.com>
2019-08-25 16:46:39 +0200

    solution to ex3

--- ch3-objects_types_and_values/exercises/exercise03/main.cpp
+++ ch3-objects_types_and_values/exercises/exercise03/main.cpp
@@ -18,8 +18,31 @@ int main()
     // main.cpp:17:17: error: definition or redeclaration of 'string' not allowed inside a function
 
 
+    //int _is_this_int_ok = 1; // ok but underscore is usually reserved implementation and system entities
 
+    //double 2x = 4;  // a name must start with a letter
+    // main.cpp:23:12: error: expected unqualified-id
 
+    //int time$to$market = 5; // gives no error although $ is not a letter, digit or underscore.
+    // no error with clan but could give errors on other compilers
+
+    //int start menu = 2; // space is not a letter, digit, or underscore
+    // main.cpp:29:14: error: expected ';' at end of declaration
+
+    //char correct? = 'c'; // ? is not a letter, digit, or underscore
+    // main.cpp:32:17: error: expected ';' at end of declaration
+
+    // The following are all legal names, which start
+    // with a letter and contains only letters, digits, and underscores.
+    double x;
+
+    int number_of_elements;
+
+    double Fourier_transform;
+
+    double z2;
+
+    char Polygon;
 
     return 0;
 }

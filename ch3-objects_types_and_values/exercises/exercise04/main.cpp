b2192924bcfc7f36b738f94c90671be9dab6ddfd
Franz Pucher <franz.pucher@gmail.com>
2019-08-25 18:46:32 +0200

    adds solution to exercise 4

--- ch3-objects_types_and_values/exercises/exercise04/main.cpp
+++ ch3-objects_types_and_values/exercises/exercise04/main.cpp
@@ -3,7 +3,36 @@
 
 int main()
 {
-    
+    cout << "Enter two integer values (followed by 'Enter')\n";
+    int val1, val2;
+    cin >> val1 >> val2;
+
+    int smaller, larger;
+    bool same = false;
+    if (val1 < val2)
+    {
+        smaller = val1;
+        larger = val2;
+    } else {
+        //if (val1 == val2)
+        //{
+        //    same = true;
+        //}
+        smaller = val2;
+        larger = val1;
+    }
+
+    int sum = smaller + larger;
+    int difference = larger - smaller;
+    int product = smaller * larger;
+    int ratio = larger/smaller;
+
+    cout << "smaller = " << smaller << "\n"
+        << "larger = " << larger  << "\n"
+        << "sum = " << sum << "\n"
+        << "difference = " << difference << "\n"
+        << "product = " << product << "\n"
+        << "ratio = " << ratio << "\n";
 
     return 0;
 }

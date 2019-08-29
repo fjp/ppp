de588e64f4498b42175ab0e54c1412a2797ccac3
Franz Pucher <franz.pucher@gmail.com>
2019-08-29 20:02:54 +0200

    adds solution to exercise 09

---
+++ ch3-objects_types_and_values/exercises/exercise09/main.cpp
@@ -0,0 +1,41 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Enter a number from 0 to 4 as a string, e.g. 'zero' (followed by 'Enter')\n";
+    string val;
+    cin >> val;
+
+    int digit = -1;
+    if ("zero" == val)
+    {
+        digit = 0;
+    }
+    else if ("one" == val)
+    {
+        digit = 1;
+    }
+    else if ("two" == val)
+    {
+        digit = 2;
+    }
+    else if ("three" == val)
+    {
+        digit = 3;
+    }
+    else if ("four" == val)
+    {
+        digit = 4;
+    }
+
+    if (-1 != digit)
+    {
+        cout << "The spelled-out number " << val << " corresponds to " << digit << ".\n";
+    }
+    else
+    {
+        cout << "not a number I know\n"
+    }
+        
+    return 0;
+}

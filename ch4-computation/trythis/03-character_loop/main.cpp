46784077c7cb9ddc42484732460cd2eb3fd9f355
Franz Pucher <franz.pucher@gmail.com>
2019-09-01 19:58:23 +0200

    adds solution for try this 03 of ch4

--- ch4-computation/trythis/03-character_loop/main.cpp
+++ ch4-computation/trythis/03-character_loop/main.cpp
@@ -3,5 +3,13 @@
 int main() 
 {
 
+    int i = 0;
+
+    while (i<26) // the loop condition testing the loop variable i
+    {
+        int val = 'a' + i;
+        cout << char(val) << '\t' << int(val) << '\n';
+        ++i ; // increment the loop variable i
+    }
     return 0;
 }

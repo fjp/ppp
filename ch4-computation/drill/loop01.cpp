19c5dc0419c3941429c012fae488e911d8669d24
Franz Pucher <franz.pucher@gmail.com>
2019-09-01 22:42:46 +0200

    fix naming

--- ch4-computation/drill/loop01.cpp
+++ ch4-computation/drill/loop01.cpp
@@ -4,16 +4,7 @@ int main() {
     int val1, val2;
     while (cin >> val1 >> val2)
     {
-        if (val1 < val2)
-        {
-            cout << "The smaller value is: " << val1 << '\n';
-            cout << "The larger value is: " << val2 << '\n';
-        }
-        else
-        {
-            cout << "The smaller value is: " << val2 << '\n';
-            cout << "The larger value is: " << val1 << '\n';
-        }
+        cout << val1 << '\t' << val2 << '\n';
     }
 
     return 0;

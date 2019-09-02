2fe646ac5072c4f3c53b72e05ba62b422e332af6
Franz Pucher <franz.pucher@gmail.com>
2019-09-02 22:36:41 +0200

    adds output to drill 01:

--- ch4-computation/drill/loop01.cpp
+++ ch4-computation/drill/loop01.cpp
@@ -1,6 +1,9 @@
 #include "std_lib_facilities.h"
 
 int main() {
+
+    cout << "Enter two integer values seperated by a space (followed by 'Enter'):\n";
+
     int val1, val2;
     while (cin >> val1 >> val2)
     {

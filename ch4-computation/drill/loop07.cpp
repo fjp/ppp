d2a2785e782cb0cbbdbac3663c0cab381f7caa7f
Franz Pucher <franz.pucher@gmail.com>
2019-09-02 22:16:13 +0200

    adds function to convert from each unit to meter

--- ch4-computation/drill/loop07.cpp
+++ ch4-computation/drill/loop07.cpp
@@ -1,6 +1,9 @@
 #include "std_lib_facilities.h"
 
 
+constexpr double cm_to_m {0.01};
+constexpr double in_to_m {2.54*cm_to_m};
+constexpr double ft_to_m {12.0*in_to_m};
 const vector<string> legal_units {"cm", "m", "in", "ft"};
 
 bool legalUnit(string unit)
@@ -25,10 +28,30 @@ void printLegalUnits()
             << "\tft for feet\n";
 }
 
+double convertToMeter(double val, string unit)
+{
+    if ("cm" == unit)
+    {
+        return val * cm_to_m;
+    }
+    else if ("in" == unit)
+    {
+        return val * in_to_m;
+    }
+    else if ("ft" == unit)
+    {
+        return val * ft_to_m;
+    }
+    else {
+        return val;
+    }
+}
+
 
 int main() {
     bool first {true};
     double val {0.0};
+    double valMeter {0.0};
     double smallest {0.0};
     double largest {0.0};
     string unit {" "};
@@ -41,7 +64,12 @@ int main() {
 
         if (legalUnit(unit))
         {
+            valMeter = convertToMeter(val, unit);
             cout << val << unit;
+            if (unit != "m")
+            {
+                cout << " (" << valMeter << "m)";
+            }
 
             if (first == true)
             {
@@ -51,15 +79,15 @@ int main() {
 
                 cout << " is the first value and therefore the smallest and largest so far.\n";
             }
-            else if (val < smallest)
+            else if (valMeter < smallest)
             {
                 cout << " the smallest so far.\n";
-                smallest = val;
+                smallest = valMeter;
             }
-            else if (val > largest)
+            else if (valMeter > largest)
             {
                 cout << " the largest so far.\n";
-                largest = val;
+                largest = valMeter;
             }
             else
             {

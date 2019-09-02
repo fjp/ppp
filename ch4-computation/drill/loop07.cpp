8b1efa75977104a33baf1019587750531f4532fe
Franz Pucher <franz.pucher@gmail.com>
2019-09-02 21:59:38 +0200

    fixes bugs in drill 07 and adds useful functions

--- ch4-computation/drill/loop07.cpp
+++ ch4-computation/drill/loop07.cpp
@@ -1,29 +1,74 @@
 #include "std_lib_facilities.h"
 
+
+const vector<string> legal_units {"cm", "m", "in", "ft"};
+
+bool legalUnit(string unit)
+{
+    bool legal = false;
+    for (auto legal_unit : legal_units)
+    {
+        if (unit == legal_unit)
+        {
+            legal = true;
+        }
+
+    }
+    return legal;
+}
+
+void printLegalUnits()
+{
+    cout << "\tcm for centimeters\n"
+            << "\tm for meters\n"
+            << "\tin for inches\n"
+            << "\tft for feet\n";
+}
+
+
 int main() {
-    bool first = true;
-    double val;
-    string unit;
+    bool first {true};
+    double val {0.0};
+    double smallest {0.0};
+    double largest {0.0};
+    string unit {" "};
+
+    cout << "Enter a double value followed by a unit with or without a space in between (followed by 'Enter'):\n";
+    printLegalUnits();
+
     while (cin >> val >> unit)
     {
-        if (first == true)
+
+        if (legalUnit(unit))
         {
-            first = false;
-            smallest = val;
-            largest = val;
+            cout << val << unit;
+
+            if (first == true)
+            {
+                first = false;
+                smallest = val;
+                largest = val;
+
+                cout << " is the first value and therefore the smallest and largest so far.\n";
+            }
+            else if (val < smallest)
+            {
+                cout << " the smallest so far.\n";
+                smallest = val;
+            }
+            else if (val > largest)
+            {
+                cout << " the largest so far.\n";
+                largest = val;
+            }
+            else
+            {
+                cout << '\n';
+            }
         }
-        if (unit == "cm" || unit == "m" || unit == "in" || unit == "ft")
-        {
-            cout << val << unit '\n';
-            
-            if (val < smallest)
-            {
-                cout << "The smalles so far.\n";
-            }
-            if (val > largest)
-            {
-                cout << "The largest so far\n";
-            }
+        else {
+            cout << "Error: no legal unit. Enter one of \n";
+            printLegalUnits();
         }
     }
 

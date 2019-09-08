4db219e64bbc16d673002a15cfd24ca1035c8199
Franz Pucher <franz.pucher@gmail.com>
2019-09-08 17:34:33 +0200

    adds mean computation from original program

--- ch4-computation/exercises/exercise02/main.cpp
+++ ch4-computation/exercises/exercise02/main.cpp
@@ -9,6 +9,12 @@ int main()
     for (double temp; cin>>temp; ) // read into temp
         temps.push_back(temp);  // put temp into vector
 
+
+    // compute mean temperature:
+    double sum = 0;
+    for (int x : temps) sum += x;
+    cout << "Average temperature: " << sum/temps.size() << '\n';
+
     // compute median temperature:
     sort(temps); // sort temperatures
     cout << "Median temperature: " << temps[temps.size()/2] << '\n';

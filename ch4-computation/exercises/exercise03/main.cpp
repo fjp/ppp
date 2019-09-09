8eee183f2a30c78a18e28c16cb94ff7676457abf
F Pucher <franz.pucher@gmail.com>
2019-09-09 13:13:04 +0200

    adds solution for exercise 03 of chapter 4

---
+++ ch4-computation/exercises/exercise03/main.cpp
@@ -0,0 +1,28 @@
+#include "std_lib_facilities.h"
+
+// compute median temperatures int 
+int main()
+{
+    cout << "Enter a series of double values, which represent the distance between two cities\n"
+            "(followed by '|' or a another non double/integer character):\n";
+
+    vector<double> distances; // city distances
+    for (double ditance; cin >> distance; ) // read into distance
+        distances.push_back(distance);  // put distance into vector
+
+
+    // compute total distance:
+    double sum {0.0};
+    for (double distance : distances) sum += x;
+    cout << "Total distance: " << sum/distances.size() << '\n';
+
+    // compute smallest and largest distance:
+    sort(distances); // sort distances
+    cout << "Smalles distance: " << distances[0] << '\n'
+         << "Largest distance: " << distances[distances.size()-1] << '\n';
+         
+         
+    cout << "The mean distance between two cities is: " << sum/distances.size() << '\n';
+
+    return 0;
+}

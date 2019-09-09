f192bd0ad430263faee079c27d841f821167a6d5
Franz Pucher <franz.pucher@gmail.com>
2019-09-09 19:04:42 +0200

    fix pull conflict

---
+++ ch4-computation/exercises/exercise03/main.cpp
@@ -0,0 +1,23 @@
+#include "std_lib_facilities.h"
+
+// compute median temperatures int 
+int main()
+{
+    cout << "Enter a series of temperatures to get the median (followed by '|' or a another non double/integer character):\n";
+
+    vector<double> temps; // temperatures
+    for (double temp; cin>>temp; ) // read into temp
+        temps.push_back(temp);  // put temp into vector
+
+
+    // compute mean temperature:
+    double sum = 0;
+    for (int x : temps) sum += x;
+    cout << "Average temperature: " << sum/temps.size() << '\n';
+
+    // compute median temperature:
+    sort(temps); // sort temperatures
+    cout << "Median temperature: " << temps[temps.size()/2] << '\n';
+
+    return 0;
+}
---
+++ ch4-computation/exercises/exercise03/main_solution.cpp
@@ -0,0 +1,30 @@
+#include "std_lib_facilities.h"
+
+// compute median temperatures int 
+int main()
+{
+    cout << "Enter a series of temperatures to get the median (followed by '|' or a another non double/integer character):\n";
+
+    vector<double> temps; // temperatures
+    for (double temp; cin>>temp; ) // read into temp
+        temps.push_back(temp);  // put temp into vector
+
+
+    // compute mean temperature:
+    double sum = 0;
+    for (int x : temps) sum += x;
+    cout << "Average temperature: " << sum/temps.size() << '\n';
+
+    // compute median temperature:
+    sort(temps); // sort temperatures
+
+    double median = -1;
+    if (temps.size() > 1 && 0 == temps.size() % 2)
+        median = (temps[temps.size()/2 - 1] + temps[temps.size()/2]) / 2.0;
+    else
+        median = temps[temps.size()/2];
+
+    cout << "Median temperature: " << median << '\n';
+
+    return 0;
+}

4940c15be8a70e39b9794ef050716c60435c4c06
Franz Pucher <franz.pucher@gmail.com>
2019-09-09 22:37:31 +0200

    working version of guessing game

--- ch4-computation/exercises/exercise04/main.cpp
+++ ch4-computation/exercises/exercise04/main.cpp
@@ -1,10 +1,8 @@
 #include "std_lib_facilities.h"
 
-// Number guessing program
+// Number guessing game
 int main()
 {
-    cout << "Think of a number between 1 and 100\n\n";
-
     int number {50};
 
     // define upper and lower bounds
@@ -13,38 +11,61 @@ int main()
     int range {upper - lower};
 
     char answer {'\0'};
-    int question {1};
+    int question {0};
+
+    cout << "Think of a number between " << lower << " and " << upper << "\n\n";
     while (lower != upper)
     {
-        cout << question++ << ". Is the number you are thinking of " << number << "? (Enter 'y' or 'n') \n";
-        cin >> answer;
-        if ('y' == answer)
+
+        range = upper - lower;
+        if (range == 1)
+            number = upper;
+        else
+            number = lower + range/2;
+
+        //cout << "upper: " << upper << " lower: " << lower << " range: " << range << '\n';
+
+        if (question%2 == 0)
         {
-            lower = number;
-            upper = number;
+            cout << question + 1 << ". Is the number you are thinking of less than " << number << "? (Enter 'y' or 'n') \n";
 
-        } else if ('n' == answer) {
-
-            cout << question++ << ". Is the number you are thinking of less than " << number << "? (Enter 'y' or 'n') \n";
             cin >> answer;
             if ('y' == answer)
             {
-                upper = number;
-                range = upper - lower;
-                number = lower + range/2;
-            } else {
-                lower = number;
-                range = upper - lower;
-                number = lower + range/2;
+                upper = number-1;
+                question++;
 
+            } else if ('n' == answer) {
+                lower = number;
+                question++;
+
+            } else {
+                cout << "Please enter 'y' or 'n' ...\n";
             }
         } else {
-            cout << "Please enter 'y' or 'n' ...\n";
+
+            cout << question + 1 << ". Is the number you are thinking of greater than " << number << "? (Enter 'y' or 'n') \n";
+            cin >> answer;
+            if ('y' == answer)
+            {
+                lower = number+1;
+                question++;
+
+            } else if ('n' == answer) {
+                upper = number;
+                question++;
+
+            } else {
+                cout << "Please enter 'y' or 'n' ...\n";
+            }
+
         }
 
     }
 
-    cout << "Is the number you guessed " << number << "?\n";
+
+    cout << "The number you are thinking of is " << lower << "\n";
+    cout << "I needed " << question << " guesses.\n";
 
     return 0;
 }

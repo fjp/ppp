c3ebc248ba31a4ed0aae06b9f3204ec2b9667557
F Pucher <franz.pucher@gmail.com>
2019-09-10 17:12:30 +0200

    copys main.cpp from ex 5 of ch4

---
+++ ch4-computation/exercises/exercise07/main.cpp
@@ -0,0 +1,42 @@
+#include "std_lib_facilities.h"
+
+// Simple calculator
+int main()
+{
+    string input {"Enter three arguments: two double operands and a character ('+','-','*','/') representing an operation (followed by 'Enter').\n"};
+    
+    cout << input;
+    
+    double op1, op2;
+    char operation {'\0'};
+   
+    while (cin >> op1 >> op2 >> operation)
+    {
+        switch (operation)
+        {
+            case '+':
+                cout << "The sum of " << op1 << " and " << op2 << " is "; << op1+op2 << '\n';
+                break;
+            case '-':
+                cout << "The difference of " << op1 << " and " << op2 << " is "; << op1-op2 << '\n';
+                break;
+            case '*':
+                cout << "The product of " << op1 << " and " << op2 << " is "; << op1*op2 << '\n';
+                break;
+            case '/':
+                if (op2 == 0)
+                        cout << "ERROR: Division by zero\n";
+                    else
+                        cout << "The division of " << op1 << " and " << op2 << " is " << op1/op2 << '\n';
+                break;
+            default:
+                cout << "The operator " << operation << " is not supported!\n";
+                break;
+        }
+        
+        cout << input;
+    }
+    
+    
+    return 0;
+}

ee43d1ba68b5b7d04cc6ee88e6db6095bc9e0873
F Pucher <franz.pucher@gmail.com>
2019-09-10 12:51:09 +0200

    adds functionality to read operands and opertor

--- ch4-computation/exercises/exercise05/main.cpp
+++ ch4-computation/exercises/exercise05/main.cpp
@@ -3,6 +3,12 @@
 // Simple calculator
 int main()
 {
+    cout << "Enter three arguments: two double values and a character representing an operation.\n";
+    
+    double val1, val2;
+    char op {'\0'};
+    cin >> val1 >> val2 >> op;
+    
     
 
     return 0;

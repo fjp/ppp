950333a49e7a3462408843f88cb0b49051d69a5d
F Pucher <franz.pucher@gmail.com>
2019-09-10 15:36:22 +0200

    adds solution to exercise 06 of ch 4

--- ch4-computation/exercises/exercise06/main.cpp
+++ ch4-computation/exercises/exercise06/main.cpp
@@ -3,7 +3,15 @@
 // Convert spelled-out digits
 int main()
 {
+    vector<string> stringDigits {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     
+    cout << "Enter integer digits from 0 to 9 which will be converted to spelled-out digits (follwed by 'Enter')\n";
+    
+    int value;
+    while (cin >> value)
+    {
+        cout << stringDigits[value] << '\n';
+    }
     
     return 0;
 }

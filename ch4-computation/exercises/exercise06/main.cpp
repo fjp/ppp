5e70c2ed4997c8b21a1423864f393ddc66350fb6
Franz Pucher <franz.pucher@gmail.com>
2019-09-10 20:24:01 +0200

    adds output if value is not in range

--- ch4-computation/exercises/exercise06/main.cpp
+++ ch4-computation/exercises/exercise06/main.cpp
@@ -5,12 +5,15 @@ int main()
 {
     vector<string> stringDigits {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     
-    cout << "Enter integer digits from 0 to 9 which will be converted to spelled-out digits (follwed by 'Enter')\n";
+    cout << "Enter integer digits from 0 to 9 which will be converted to spelled-out digits (followed by 'Enter')\n";
     
     int value;
     while (cin >> value)
     {
-        cout << stringDigits[value] << '\n';
+        if (0 <= value && value <= 9)
+            cout << stringDigits[value] << '\n';
+        else
+            cout << "Error: value must be an integer between 0 and 9.\n";
     }
     
     return 0;

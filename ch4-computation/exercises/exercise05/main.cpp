8db5104909eb82d95015b2831e5b90f112047e97
F Pucher <franz.pucher@gmail.com>
2019-09-10 12:57:44 +0200

    adds solution to exercise 5 of ch4

--- ch4-computation/exercises/exercise05/main.cpp
+++ ch4-computation/exercises/exercise05/main.cpp
@@ -3,13 +3,36 @@
 // Simple calculator
 int main()
 {
-    cout << "Enter three arguments: two double values and a character representing an operation.\n";
+    cout << "Enter three arguments: two double operands and a character ('+','-','*','/') representing an operation (followed by 'Enter').\n";
     
-    double val1, val2;
-    char op {'\0'};
+    double op1, op2;
+    char operation {'\0'};
     cin >> val1 >> val2 >> op;
     
-    
 
+    cin >> op1 >> op2 >> operation;
+    
+    double result = 0;
+    switch (operation)
+    {
+        case '+':
+            result = op1+op2;
+            break;
+        case '-':
+            result = op1-op2;
+            break;
+        case '*':
+            result = op1*op2;
+            break;
+        case '/':
+            result = op1/op2;
+            break;
+        default:
+            result = op1+op2;
+            break;
+    }
+    
+    cout << "The result of " << op1 << " " << operation << " " << op2 << " is " << result << "\n";
+    
     return 0;
 }

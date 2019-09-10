4b40f56bdc74147e889148923516926aaa77540f
F Pucher <franz.pucher@gmail.com>
2019-09-10 13:06:15 +0200

    adds while loop to get repeated inputs

--- ch4-computation/exercises/exercise05/main.cpp
+++ ch4-computation/exercises/exercise05/main.cpp
@@ -3,34 +3,38 @@
 // Simple calculator
 int main()
 {
-    cout << "Enter three arguments: two double operands and a character ('+','-','*','/') representing an operation (followed by 'Enter').\n";
+    string input {"Enter three arguments: two double operands and a character ('+','-','*','/') representing an operation (followed by 'Enter').\n"};
+    
+    cout << input;
     
     double op1, op2;
     char operation {'\0'};
-    cin >> val1 >> val2 >> op;
-    
-    cin >> op1 >> op2 >> operation;
-    
-    switch (operation)
+   
+    while (cin >> op1 >> op2 >> operation)
     {
-        case '+':
-            cout << "The sum of " << op1 << " and " << op2 << " is "; << op1+op2 << '\n';
-            break;
-        case '-':
-            cout << "The difference of " << op1 << " and " << op2 << " is "; << op1-op2 << '\n';
-            break;
-        case '*':
-            cout << "The product of " << op1 << " and " << op2 << " is "; << op1*op2 << '\n';
-            break;
-        case '/':
-            if (op2 == 0)
-                    cout << "ERROR: Division by zero\n";
-                else
-                    cout << "The division of " << op1 << " and " << op2 << " is " << op1/op2 << '\n';
-            break;
-        default:
-            cout << "The operator " << operation << " is not supported!\n";
-            break;
+        switch (operation)
+        {
+            case '+':
+                cout << "The sum of " << op1 << " and " << op2 << " is "; << op1+op2 << '\n';
+                break;
+            case '-':
+                cout << "The difference of " << op1 << " and " << op2 << " is "; << op1-op2 << '\n';
+                break;
+            case '*':
+                cout << "The product of " << op1 << " and " << op2 << " is "; << op1*op2 << '\n';
+                break;
+            case '/':
+                if (op2 == 0)
+                        cout << "ERROR: Division by zero\n";
+                    else
+                        cout << "The division of " << op1 << " and " << op2 << " is " << op1/op2 << '\n';
+                break;
+            default:
+                cout << "The operator " << operation << " is not supported!\n";
+                break;
+        }
+        
+        cout << input;
     }
     
     

360133d1e165dbc8aa28b08b01fb5759bf2f299a
F Pucher <franz.pucher@gmail.com>
2019-09-11 17:03:09 +0200

    adds solution to exercise 08 of ch 4

--- ch4-computation/exercises/exercise08/main.cpp
+++ ch4-computation/exercises/exercise08/main.cpp
@@ -2,7 +2,22 @@
 
 int main()
 {
-
+    int nDesiredGrains {1000};
+    
+    int nSquare {1};
+    
+    int nGrainsAtCurSquare{1};
+    int nTotalGrains {1};
+    
+    while (nDesiredGrains > nTotalGrains)
+    {
+        nGrainsAtCurSquare *= 2;
+        nTotalGrains += nGrainsAtCurSquare;
+        
+        cout << "Square: " << nSquare << '\n';
+        cout << "Grains at current square: " << nGrainsAtCurSquare << '\n';
+        cout << "Total grains: " << nTotalGrains << '\n';
+    }
 
     return 0;
 }

2a79c8af4288bbd088fc3ef2291890b871a1aace
Franz Pucher <franz.pucher@gmail.com>
2019-09-11 22:00:56 +0200

    adds solution to exercise 09 of ch4

--- ch4-computation/exercises/exercise09/main.cpp
+++ ch4-computation/exercises/exercise09/main.cpp
@@ -1,34 +1,38 @@
 #include "std_lib_facilities.h"
 
 int main() {
-    vector<int> naDesiredRice{1000, 1'000'000, 1'000'000'000};
-
-    int nSquare {1};
 
     int nRiceOnCurrSquare {1};
     int nRiceOnPrevSquares {0};
 
-    for (int i = 0; i < naDesiredRice.size(); ++i) {
-        while (naDesiredRice[i] > nRiceOnPrevSquares + nRiceOnCurrSquare) {
+    double dRiceOnCurrSquare {1.0};
+    double dRiceOnPrevSquares {0.0};
+
+    // Maximum number that fits in an int (assuming 32 bits)
+    // 2^32/2: form −(2^31) to 2^31 − 1 => maximum is 2,147,483,647
+
+    // Maximum number that fits in a double (assuming 64 bit)
+    // 2^64/2: from −(2^63) to 2^63 − 1 => 9,223,372,036,854,775,807
 
 
-            cout << "Square: " << nSquare++ << '\n';
-            cout << "\tGrains at current square: " << nRiceOnCurrSquare << '\n';
-            cout << "\tRice on previous squares: " << nRiceOnPrevSquares << '\n';
-            cout << "\tTotal rice grains: " << nRiceOnPrevSquares + nRiceOnCurrSquare << '\n';
 
-            nRiceOnPrevSquares += nRiceOnCurrSquare;
-            nRiceOnCurrSquare *= 2;
-        }
+    for (int nSquare = 1; nSquare <= 64; ++nSquare) {
 
-        cout << "To give the inventor at least " << naDesiredRice[i] << " grains, " << nSquare << " squares are required.\n";
-        cout << "Grains at square " << nSquare << ": " << nRiceOnCurrSquare << '\n';
-        cout << "With the rice on the previous squares this results in " << nRiceOnPrevSquares + nRiceOnCurrSquare << " total grains for the inventor.\n\n";
+        cout << "Square: " << nSquare << '\n';
+        cout << "\tGrains on current square [in]: " << nRiceOnCurrSquare << ", [double]: " << dRiceOnCurrSquare << '\n';
+        cout << "\tRice on previous squares [int]: " << nRiceOnPrevSquares << ", [double]: " << dRiceOnPrevSquares << '\n';
+        cout << "\tTotal rice grains [int]: " << nRiceOnPrevSquares + nRiceOnCurrSquare << ", [double]: " << dRiceOnPrevSquares + dRiceOnCurrSquare << '\n';
 
-        nSquare = 1;
+        nRiceOnPrevSquares += nRiceOnCurrSquare;
+        nRiceOnCurrSquare *= 2;
 
-        nRiceOnCurrSquare = 1;
-        nRiceOnPrevSquares = 0;
+        dRiceOnPrevSquares += dRiceOnCurrSquare;
+        dRiceOnCurrSquare *= 2;
+
     }
+
+        //cout << "To give the inventor at least " << naDesiredRice[i] << " grains, " << nSquare << " squares are required.\n";
+        //cout << "Grains at square " << nSquare << ": " << nRiceOnCurrSquare << '\n';
+        //cout << "With the rice on the previous squares this results in " << nRiceOnPrevSquares + nRiceOnCurrSquare << " total grains for the inventor.\n\n";
     return 0;
 }

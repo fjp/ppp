b309938b61e7ddfe1cb7bf9cb3d892e4b05638ea
Franz Pucher <franz.pucher@gmail.com>
2019-09-12 21:47:55 +0200

    adds first solution of exercise 13

--- ch4-computation/exercises/exercise13/main.cpp
+++ ch4-computation/exercises/exercise13/main.cpp
@@ -1,9 +1,19 @@
 #include "std_lib_facilities.h"
 
 
+bool Sived(int i_nNumber, vector<int> i_naSived)
+{
+    for (int nSived : i_naSived)
+    {
+        if (nSived == i_nNumber)
+            return true;
+    }
+    return false;
+}
+
 int main() {
 
-    vector<int> naPrimes {2};
+    vector<int> naPrimes {};
 
     //cout << "Enter an integer greater than 1 that defines the maximum of a range for which primes are searched.\n";
 
@@ -17,12 +27,30 @@ int main() {
     //        cout << "Enter an integer greater than 1 (followed by 'Enter')\n";
     //}
 
-    for (int nNumber = 1; nNumber <= 100; ++nNumber)
+    vector<int> naSived {};
+    int nComposite {0};
+    int nCount {1};
+    for (int nNumber = 2; nNumber <= 100; ++nNumber)
     {
-        
+        if (!Sived(nNumber, naSived)) {
+            naPrimes.push_back(nNumber);
+            // Add multiples of the current prime to the sived vector
+            for (int nMultiplier = 1; nMultiplier * nNumber <= 100; ++nMultiplier) {
+                nComposite = nNumber * nMultiplier;
+                naSived.push_back(nComposite);
+                cout << nComposite << " ";
+                if (nCount % 10 == 0) {
+                    cout << '\n';
+                }
+                nCount++;
+            }
+        }
+        else {
+            cout << "\n";
+        }
     }
 
-    cout << "Found prime numbers between 1 and " << nMax << ": \n";
+    cout << "Found prime numbers between 1 and " << 100 << ": \n";
     int nPrime {2};
     for (int nIdx = 0; nIdx < naPrimes.size(); ++nIdx)
     {

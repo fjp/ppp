2a22fd7e7d97ad70dc208bb1cd19decace3b3870
Franz Pucher <franz.pucher@gmail.com>
2019-09-12 20:26:01 +0200

    changes output format

--- ch4-computation/exercises/exercise12/main.cpp
+++ ch4-computation/exercises/exercise12/main.cpp
@@ -16,7 +16,19 @@ int main() {
 
     vector<int> naPrimes {2};
 
-    for (int nNumber = 1; nNumber <= 100; ++nNumber)
+    cout << "Enter an integer greater than 1 that defines the maximum of a range for which primes are searched.\n";
+
+    int nMax;
+    bool bValidInput {false};
+    while (!bValidInput)
+    {
+        if (cin >> nMax && nMax > 1)
+            bValidInput = true;
+        else
+            cout << "Enter an integer greater than 1 (followed by 'Enter')\n";
+    }
+
+    for (int nNumber = 1; nNumber <= nMax; ++nNumber)
     {
         if (IsPrime(nNumber, naPrimes))
         {
@@ -24,10 +36,14 @@ int main() {
         }
     }
 
-    cout << "Found prime numbers between 1 and 100: \n";
-    for (int nPrime : naPrimes)
+    cout << "Found prime numbers between 1 and " << nMax << ": \n";
+    int nPrime {2};
+    for (int nIdx = 0; nIdx < naPrimes.size(); ++nIdx)
     {
+        nPrime = naPrimes[nIdx];
         cout << nPrime << " ";
+        if ((nIdx+1) % 25 == 0)
+            cout << '\n';
     }
     cout << '\n';
 

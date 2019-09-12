dff65e304ae70cecb89f8d92a77814bf6cdfa579
F Pucher <franz.pucher@gmail.com>
2019-09-12 09:47:26 +0200

    adds function to get player gesture

--- ch4-computation/exercises/exercise10/main.cpp
+++ ch4-computation/exercises/exercise10/main.cpp
@@ -1,5 +1,30 @@
 #include "std_lib_facilities.h"
 
+
+const vector<string> straGesture {"Rock", "Paper", "Scissors"};
+
+string PlayerThrow()
+{
+    string strPlayerThrow;
+    while (true)
+    {
+        if (cin >> strPlayerThrow)
+        {
+            for (string strGesture : straGesture)
+            {
+                if (strGesture == strPlayerThrow)
+                    return strPlayerThrow;
+            }
+        }
+        else 
+        {
+            cout << "Please try again. Enter 'Rock', 'Paper' or 'Scissors'\n";
+            cin.clear();   
+        }
+    }
+}
+
+
 int main() {
 
 

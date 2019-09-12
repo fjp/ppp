f9ab65e04ba91446b9d764b9f1ba4967c3a4ac8c
Franz Pucher <franz.pucher@gmail.com>
2019-09-12 19:48:34 +0200

    adds solution to exercise11

--- ch4-computation/exercises/exercise11/main.cpp
+++ ch4-computation/exercises/exercise11/main.cpp
@@ -1,7 +1,35 @@
 #include "std_lib_facilities.h"
 
+
+// Check if the number can be divided by a prime number smaller than itself
+bool IsPrime(int i_nNumber, vector<int> i_naPrimes)
+{
+    for (int prime : i_naPrimes)
+    {
+        if (i_nNumber < prime || i_nNumber % prime == 0)
+            return false;
+    }
+    return true;
+}
+
 int main() {
 
+    vector<int> naPrimes {2};
+
+    for (int nNumber = 1; nNumber <= 100; ++nNumber)
+    {
+        if (IsPrime(nNumber, naPrimes))
+        {
+            naPrimes.push_back(nNumber);
+        }
+    }
+
+    cout << "Found prime numbers between 1 and 100: \n";
+    for (int nPrime : naPrimes)
+    {
+        cout << nPrime << " ";
+    }
+    cout << '\n';
 
     return 0;
 }

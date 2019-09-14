fdb11419199e9da121065a3b318fd1ea34db6d01
Franz Pucher <franz.pucher@gmail.com>
2019-09-14 17:58:06 +0200

    adds solution for ex15

--- ch4-computation/exercises/exercise15/main.cpp
+++ ch4-computation/exercises/exercise15/main.cpp
@@ -16,27 +16,30 @@ int main() {
 
     vector<int> naPrimes {2};
 
-    cout << "Enter an integer greater than 1 that defines the maximum of a range for which primes are searched.\n";
+    cout << "Enter an integer greater than 1 that defines the number of primes from 1 to n that are searched.\n";
 
-    int nMax;
+    int nN;
     bool bValidInput {false};
     while (!bValidInput)
     {
-        if (cin >> nMax && nMax > 1)
+        if (cin >> nN && nN > 1)
             bValidInput = true;
         else
             cout << "Enter an integer greater than 1 (followed by 'Enter')\n";
     }
 
-    for (int nNumber = 1; nNumber <= nMax; ++nNumber)
+    int nNumber {1};
+    while (naPrimes.size() < nN)
     {
         if (IsPrime(nNumber, naPrimes))
         {
             naPrimes.push_back(nNumber);
         }
+        nNumber++;
+
     }
 
-    cout << "Found prime numbers between 1 and " << nMax << ": \n";
+    cout << "Found "<< nN << " prime numbers between 1 and " << naPrimes[naPrimes.size()-1] << ": \n";
     int nPrime {2};
     for (int nIdx = 0; nIdx < naPrimes.size(); ++nIdx)
     {

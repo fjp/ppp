81bd1a9af7d0df7138aceb26ce9ef5c3829ce1dd
F Pucher <franz.pucher@gmail.com>
2019-09-15 21:08:10 +0200

    adds initial main.cpp of ex18 of ch4

---
+++ ch4-computation/exercises/exercise18/main.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+int main() {
+
+    cout << "Enter the coefficients 'a', 'b' and 'c' as double to get the results of a quadratic equation (Followed by 'Enter'):\n";
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    int x1, x2;
+    
+    
+    
+    return 0;
+}

2c4a55240764d133e21753f4e9c2b98e1ae56098
F Pucher <franz.pucher@gmail.com>
2019-09-15 22:25:05 +0200

    adds initial main.cpp of ex19 of ch4

---
+++ ch4-computation/exercises/exercise19/main.cpp
@@ -0,0 +1,9 @@
+#include "std_lib_facilities.h"
+
+int main() {
+
+    cout << "Enter a set of name-and-value pairs (Terminate the input using 'NoName 0' followed by 'Enter'):\n";
+
+
+    return 0;
+}

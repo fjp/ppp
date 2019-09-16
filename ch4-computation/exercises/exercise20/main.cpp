938ee8dbfa23a688383b6bc1909ae9d384099046
Franz Pucher <franz.pucher@gmail.com>
2019-09-16 22:14:39 +0200

    adds solution for exercise 20

--- ch4-computation/exercises/exercise20/main.cpp
+++ ch4-computation/exercises/exercise20/main.cpp
@@ -13,6 +13,20 @@ bool Duplicate(string i_strName, vector<string> i_straNames)
     return false;
 }
 
+void PrintName(string i_strName, vector<string> i_straNames, vector<int> i_naScores)
+{
+    for (int nIdx = 0; nIdx < i_straNames.size(); ++nIdx)
+    {
+        if (i_straNames[nIdx] == i_strName)
+        {
+            cout << "Score for " << i_strName << " is " << i_naScores[nIdx] << '\n';
+            return;
+        }
+    }
+    cout << "name not found\n";
+    return;
+}
+
 int main() {
 
     cout << "Enter a set of name-and-value pairs, such as 'Joe 17' and 'Barbara 22 (Terminate the input using 'NoName 0' followed by 'Enter'):\n";
@@ -27,14 +41,11 @@ int main() {
     while (!bDuplicate && (cin >> strName >> nScore) && ("NoName" != strName || 0 != nScore)) // Terminate input with NoName 0
     {
         // Check that each name is unique and terminate with an error message if a name is entered twice.
-        if (Duplicate(strName, straNames))
-        {
+        if (Duplicate(strName, straNames)) {
             cout << "Duplicate detected! Names must be unique.\n";
             bDuplicate = true;
             // return -1; // depending on what terminate means (terminate program or terminate input?)
-        }
-        else
-        {
+        } else {
             straNames.push_back(strName);
             naScores.push_back(nScore);
         }
@@ -46,6 +57,16 @@ int main() {
     {
         cout << "(" << straNames[nIdx] << "," << naScores[nIdx] << ")\n";
     }
+
+
+    cout << "Write a name to get the score:\n";
+    while (cin >> strName)
+    {
+        PrintName(strName, straNames, naScores);
+
+        cout << "Write a name to get the score:\n";
+    }
+
 
     return 0;
 }

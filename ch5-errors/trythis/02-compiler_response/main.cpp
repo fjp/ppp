34f10a5c884922c5ee8def674b040bee0ded9faf
Franz Pucher <franz.pucher@gmail.com>
2019-09-18 00:46:12 +0200

    fix string quotes

--- ch5-errors/trythis/02-compiler_response/main.cpp
+++ ch5-errors/trythis/02-compiler_response/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int x0 = arena(7); // error: undeclared function
     int x1 = area(7); // error: wrong number of arguments
-    int x2 = area(“seven”,2); // error: 1st argument has a wrong type
+    int x2 = area("seven",2); // error: 1st argument has a wrong type
     
     return 0;
 }

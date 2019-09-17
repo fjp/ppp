921dfac6d0045098e8410fbaa368495bcff5ca39
Franz Pucher <franz.pucher@gmail.com>
2019-09-18 00:24:15 +0200

    adds main.cpp of first try this of ch5

--- ch5-errors/trythis/01-compiler_response/main.cpp
+++ ch5-errors/trythis/01-compiler_response/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int s1 = area(7; // error: ) missing
     int s2 = area(7) // error: ; missing
-    int s3 = area(7); // error: Int is not a type
+    Int s3 = area(7); // error: Int is not a type
     int s4 = area(ʻ7); // error: non-terminated character (’ missing)
     
     return 0;

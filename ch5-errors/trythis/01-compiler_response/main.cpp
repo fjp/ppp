e3c7f37a7f838a6e9427f8d0417e261e3712b65e
F Pucher <franz.pucher@gmail.com>
2019-09-18 00:13:28 +0200

    adds main.cpp for first try this of ch5

---
+++ ch5-errors/trythis/01-compiler_response/main.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+int area(int length, int width); // calculate area of a rectangle
+
+int main()
+{
+    int s1 = area(7; // error: ) missing
+    int s2 = area(7) // error: ; missing
+    int s3 = area(7); // error: Int is not a type
+    int s4 = area(ʻ7); // error: non-terminated character (’ missing)
+    
+    return 0;
+}

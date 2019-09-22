cf863d91453effd64b4875c66ac5d001660d7de7
Franz Pucher <franz.pucher@gmail.com>
2019-09-22 21:12:35 +0200

    adds solution of try this 09 of ch5

--- ch5-errors/trythis/09-5.10.1-post-conditions/main.cpp
+++ ch5-errors/trythis/09-5.10.1-post-conditions/main.cpp
@@ -1,6 +1,14 @@
 // Try This 5.10.1 Post-conditions
 // Author: Franz Pucher
 // Date: 2019.09.22
+//
+// Comments:
+//  In case of an overflow the pre-conditions are satisfied
+//  while the post-condition can fail.
+//  Here are some examples that produce an overflow for a 4 byte integer:
+//   area(60000, 60000);
+//   area(65536, 65535); // -65536
+//   area(65536, 65536); // 0
 
 #include "std_lib_facilities.h"
 
@@ -12,13 +20,32 @@ int area(int length, int width)
 {
     if (length<=0 || width <=0) error("area() pre-condition");
     int a = length*width;
-    if (a<=0) error("area() post-condition");
+    //cout << "area() a: " << a << '\n';
+    if (a<=0) error("area() post-condition"); // throw runtime_error(string s)
     return a;
 }
 
-int main()
-{
 
-    
+int main()
+try
+{
+    int length = 0;
+    int width = 0;
+
+    cout << "Enter integer length and width to get the area of the rectangle:\n"
+         << "(Negative numbers will violate the pre-conditions while large numbers\n"
+         << "will produce an overflow and violate the post-condition of area())\n";
+
+    while (cin >> length >> width)
+        cout << "Area is " << area(length, width) << '\n';
+
     return 0;
+}
+catch (runtime_error& e) {
+    cout << "Error: " << e.what() << '\n';
+    return 1;
+}
+catch (...) {
+    cout << "Error: unknown exception\n";
+    return 2;
 }

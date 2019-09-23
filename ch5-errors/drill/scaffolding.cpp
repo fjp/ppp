76a2f2321298106f3ac5f246dfed656477ababed
F Pucher <franz.pucher@gmail.com>
2019-09-23 22:12:58 +0200

    adds scaffolding of drill in ch5

---
+++ ch5-errors/drill/scaffolding.cpp
@@ -0,0 +1,20 @@
+#include "std_lib_facilities.h"
+
+int main()
+try {
+    //<<your code here>>
+    
+    
+    keep_window_open();
+    return 0;
+}
+catch (exception& e) {
+    cerr << "error: " << e.what() << '\n';
+    keep_window_open();
+    return 1;
+}
+catch (...) {
+    cerr << "Oops: unknown exception!\n";
+    keep_window_open();
+    return 2;
+}

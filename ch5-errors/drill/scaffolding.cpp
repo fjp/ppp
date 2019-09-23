2c83ef3674c9d22bf23e30306b81b5c645876fb3
Franz Pucher <franz.pucher@gmail.com>
2019-09-23 22:37:06 +0200

    adds solution of first drill

--- ch5-errors/drill/scaffolding.cpp
+++ ch5-errors/drill/scaffolding.cpp
@@ -3,7 +3,11 @@
 int main()
 try {
     //<<your code here>>
-    
+
+    // Fragment 1 (Compile-time error)
+    // Cout << "Success!\n";
+    // error: use of undeclared identifier 'Cout'; did you mean 'cout'?
+    cout << "Success!\n";
     
     keep_window_open();
     return 0;
